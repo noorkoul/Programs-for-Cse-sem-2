4d3243f5f8eb9547be6087c2658cab5c5fbe8935
noorkoul <noorkoul890@gmail.com>
2024-09-25 13:24:35 +0530

    Create q16.c

---
+++ q16.c
@@ -0,0 +1,22 @@
+//Program in C to Find sum of series: 1 2 3 4 5 .... n
+
+#include <stdio.h>
+
+int main(){
+
+    int i,n,sum;
+
+    printf("Enter n\n");
+    scanf("%d",&n);
+
+    sum=0;
+    for (i=1; i<=n; i++)
+    {
+        sum=sum+i;
+    }
+
+    printf("%d",sum);
+
+    return 0;
+
+}
