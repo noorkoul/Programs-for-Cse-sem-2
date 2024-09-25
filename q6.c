b6bebda8d7d1d90a9faa9b12c8f254f70ce25990
noorkoul <noorkoul890@gmail.com>
2024-09-25 13:07:43 +0530

    Create q6.c

---
+++ q6.c
@@ -0,0 +1,16 @@
+//Use math library (pow(), sqrt(), etc.).
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //Sqrt
+    double A;
+    A = sqrt(9);
+    printf("The sq root of 9 is %lf\n",A);
+
+    //pow
+    double B;
+    B = pow(2,3);
+    printf("The power of 2 to 3 is %lf\n",B);
