1c0f9c74d7d84779d05d9cb8f2489710ad87ee83
Noor <noorkoul890@gmail.com>
2024-09-25 21:36:56 +0530

    Update q10.c

--- q10.c
+++ q10.c
@@ -1,54 +1,36 @@
 //Program to compare three number using nesting of if-else
 #include <stdio.h>
 
-int main() {
-
-   int num1, num2, num3;
-
-   printf("Enter first number: ");
-   scanf("%d", &num1);
-   printf("Enter second number: ");
-   scanf("%d", &num2);
-   printf("Enter third number: ");
-   scanf("%d", &num3);
-
-    if (num1 == num2 && num2 == num3) {
-        printf("All three numbers are equal: %d\n", num1);
-    } else if (num1 == num2) {
-        if (num1 > num3) {
-            printf("Two numbers are equal and the largest: %d\n", num1);
-        } else {
-            printf("The largest number is: %d\n", num3);
+int main() 
+{
+    int a, b, c;
+    
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a,&b,&c);
+    
+    if (a > b) 
+    {
+        if (a > c) 
+        {
+            printf("The largest number is: %d\n", a);
+        } 
+        else 
+        {
+            printf("The largest number is: %d\n", c);
         }
-    } else if (num1 == num3) {
-        if (num1 > num2) {
-            printf("Two numbers are equal and the largest: %d\n", num1);
-        } else {
-            printf("The largest number is: %d\n", num2);
-        }
-    } else if (num2 == num3) {
-        if (num2 > num1) {
-            printf("Two numbers are equal and the largest: %d\n", num2);
-        } else {
-            printf("The largest number is: %d\n", num1);
-        }
-    } else {
-        if (num1 >= num2) {
-            if (num1 >= num3) {
-                printf("The largest number is: %d\n", num1);
-            } else {
-                printf("The largest number is: %d\n", num3);
-            }
-        } else {
-            if (num2 >= num3) {
-                printf("The largest number is: %d\n", num2);
-            } else {
-                printf("The largest number is: %d\n", num3);
-            }
+    } 
+    else 
+    {
+        if (b > c) 
+        {
+            printf("The largest number is: %d\n", b);
+        } 
+        else 
+        {
+            printf("The largest number is: %d\n", c);
         }
     }
 
-  
-
-   return 0;
+    return 0;
 }
+
