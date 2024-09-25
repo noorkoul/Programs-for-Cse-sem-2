6c13c497d5cb68cf66904af8d2f6eb4e7633beb7
noorkoul <noorkoul890@gmail.com>
2024-09-25 15:34:47 +0530

    Create q31.c

---
+++ q31.c
@@ -0,0 +1,21 @@
+//Program in C to display the following pattern
+
+// *
+// **
+// ***
+// ****
+
+
+#include <stdio.h>
+int main() {
+   
+   int i, j, space;
+   printf("Pattern 1: \n");
+   for (i = 1; i <= 4; i++) {
+    for (j = 1; j <= i; j++) {
+        printf("*");
+    }
+    printf("\n");
+   }
+ return 0;
+}
