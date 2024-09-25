6539296eeede4c86e80a1c3706d3eb2f42ba0ec4
Noor <noorkoul890@gmail.com>
2024-09-26 00:35:10 +0530

    Update q20.c

--- q20.c
+++ q20.c
@@ -1,44 +1,30 @@
 //Program to check whether a number is prime or not
 
 #include <stdio.h>
+#include <math.h>  // For sqrt function
 
-int main(){
+int main() {
+    int n, flag = 0; // Initialize flag
 
-    int i,n,flag;
+    printf("Enter a number to check if prime:\n");
+    scanf("%d", &n);
 
-    printf("Enter a number to check if prime.\n");
-    scanf("%d",&n);
-
-    if(n<=1)
-    {
-        flag=0;
-    }
-    else
-    {
-        for(int i = 2; i<n/2; i++)
-    {
-       if(n%i!=0)
-       {
-        flag=0;
-       }
-       else
-       {
-        flag =1;
-        break;
-       }
-    }
+    if (n <= 1) {
+        flag = 1; // Not prime
+    } else {
+        for (int i = 2; i <= sqrt(n); i++) { // Check up to sqrt(n)
+            if (n % i == 0) {
+                flag = 1; // Not prime
+                break; // Exit loop
+            }
+        }
     }
 
-    if(flag==0)
-    {
+    if (flag == 0) {
         printf("The number is prime.\n");
-    }
-    else
-    {
+    } else {
         printf("The number is not prime.\n");
     }
 
-
     return 0;
-
 }
