c053f2f45e926579acca9de92d060c62d804fda4
noorkoul <noorkoul890@gmail.com>
2024-09-25 18:35:22 +0530

    Create q64.c

---
+++ q64.c
@@ -0,0 +1,13 @@
+//Program in C to demonstrate use of pointers
+
+#include<stdio.h>
+
+int main() {
+
+    int *p, a;
+    a = 5;
+    p = &c;
+    printf("%d\n", *p); //output = 5
+
+    return 0;
+}
